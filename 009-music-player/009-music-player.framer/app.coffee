8dbf5ac64643b70ca2d4b2305a7a66621828da2f
Eric Kramp <mail@erickramp.com>
2017-07-23 00:18:49 -0400

    Day 9 music player complete

--- 009-music-player/009-music-player.framer/app.coffee
+++ 009-music-player/009-music-player.framer/app.coffee
@@ -1,24 +1,26 @@
+# Import file "009-music-player"
+sketch = Framer.Importer.load("imported/009-music-player@2x", scale: 1)
+btn_play_pause = sketch.btn_play_pause
+txt_track_time = sketch.track_time
+track_progress = sketch.track_group
+
 # setup play button drag behavior
+btn_play_pause.animate("default")
 btn_play_pause.draggable = true
 btn_play_pause.animationOptions =
 	curve: Spring(damping: 0.5)
 	time: 0.5
 
-icon_pause.visible = false
-
+txt_track_time.originX = 1
 txt_track_time.states.large =
 	scale: 1.5
 txt_track_time.animationOptions = 
 	time: 0.1
 
-# btn_play_pause.onTap ->
-# 	icon_play.visible = !icon_play.visible
-# 	icon_pause.visible = !icon_pause.visible
-
 btn_play_pause.onDrag ->
 	txt_track_time.animate("large")
 	if (btn_play_pause.draggable.direction == "up")
-		if (track_progress.rotationZ < 166)
+		if (track_progress.rotationZ < 176)
 			track_progress.rotationZ += 1
 	else
 		if (track_progress.rotationZ > 0)
--- 009-music-player/009-music-player.framer/framer/framer.generated.js
+++ 009-music-player/009-music-player.framer/framer/framer.generated.js
@@ -2,10 +2,501 @@
 
 
 if (!window.Framer && window._bridge) {window._bridge('runtime.error', {message:'[framer.js] Framer library missing or corrupt. Select File → Update Framer Library.'})}
+window.__imported__ = window.__imported__ || {};
+window.__imported__["009-music-player@2x/layers.json.js"] = [
+	{
+		"objectId": "B7F57A3C-7E7A-42DD-8498-7F0BBD9C20ED",
+		"kind": "artboard",
+		"name": "iPhone_7",
+		"originalName": "iPhone 7",
+		"maskFrame": null,
+		"layerFrame": {
+			"x": 234,
+			"y": 314,
+			"width": 375,
+			"height": 667
+		},
+		"visible": true,
+		"metadata": {},
+		"backgroundColor": "rgba(255, 255, 255, 1)",
+		"image": {
+			"path": "images/Layer-iPhone_7-qjdgntdb.png",
+			"frame": {
+				"x": 234,
+				"y": 314,
+				"width": 375,
+				"height": 667
+			}
+		},
+		"children": [
+			{
+				"objectId": "58BEE885-C5D4-49FD-A076-45248A6FD006",
+				"kind": "group",
+				"name": "Group",
+				"originalName": "Group",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 336,
+					"y": 75,
+					"width": 25,
+					"height": 18
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-Group-nthcruu4.png",
+					"frame": {
+						"x": 336,
+						"y": 75,
+						"width": 25,
+						"height": 18
+					}
+				},
+				"children": []
+			},
+			{
+				"objectId": "C7D66AC4-B5D2-489A-B73B-E4F73F231FC5",
+				"kind": "group",
+				"name": "track_time",
+				"originalName": "track_time",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 112,
+					"y": 437,
+					"width": 61,
+					"height": 32
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-track_time-qzdenjzb.png",
+					"frame": {
+						"x": 112,
+						"y": 437,
+						"width": 61,
+						"height": 32
+					}
+				},
+				"children": []
+			},
+			{
+				"objectId": "A6EEBA9B-352A-4681-95E6-57C1D6D4E146",
+				"kind": "group",
+				"name": "btn_shuffle",
+				"originalName": "btn_shuffle",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 321,
+					"y": 60,
+					"width": 52,
+					"height": 52
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-btn_shuffle-qtzfrujb.png",
+					"frame": {
+						"x": 321,
+						"y": 60,
+						"width": 52,
+						"height": 52
+					}
+				},
+				"children": []
+			},
+			{
+				"objectId": "1D87DBD0-3F5C-498B-A1F9-CA96BE7D7822",
+				"kind": "group",
+				"name": "btn_shuffle1",
+				"originalName": "btn_shuffle",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 268,
+					"y": 60,
+					"width": 52,
+					"height": 52
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-btn_shuffle-muq4n0rc.png",
+					"frame": {
+						"x": 268,
+						"y": 60,
+						"width": 52,
+						"height": 52
+					}
+				},
+				"children": [
+					{
+						"objectId": "63D66823-B0E9-4A71-92AE-8A644686B78C",
+						"kind": "group",
+						"name": "Group_3",
+						"originalName": "Group 3",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 285,
+							"y": 75,
+							"width": 23,
+							"height": 18
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"children": [
+							{
+								"objectId": "6B73004D-60E8-4C48-B2AD-0A3E546A8633",
+								"kind": "group",
+								"name": "Group_2_Copy",
+								"originalName": "Group 2 Copy",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 285,
+									"y": 77,
+									"width": 23,
+									"height": 16
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-Group_2_Copy-nki3mzaw.png",
+									"frame": {
+										"x": 285,
+										"y": 77,
+										"width": 23,
+										"height": 16
+									}
+								},
+								"children": []
+							},
+							{
+								"objectId": "B8D0391B-DDFD-44B5-9964-253A1B7D15C0",
+								"kind": "group",
+								"name": "Group_2",
+								"originalName": "Group 2",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 285,
+									"y": 75,
+									"width": 23,
+									"height": 16
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-Group_2-qjhemdm5.png",
+									"frame": {
+										"x": 285,
+										"y": 75,
+										"width": 23,
+										"height": 16
+									}
+								},
+								"children": []
+							}
+						]
+					}
+				]
+			},
+			{
+				"objectId": "3C46DC9E-5F0C-4E15-B056-377EE368BABA",
+				"kind": "group",
+				"name": "header",
+				"originalName": "header",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": -4,
+					"y": -2,
+					"width": 383,
+					"height": 64
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-header-m0m0nkrd.png",
+					"frame": {
+						"x": -4,
+						"y": -2,
+						"width": 383,
+						"height": 64
+					}
+				},
+				"children": []
+			},
+			{
+				"objectId": "36FE1421-CB89-49C2-9803-1C94D91D9C3D",
+				"kind": "group",
+				"name": "play_btn_group",
+				"originalName": "play_btn_group",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 193,
+					"y": 347,
+					"width": 200,
+					"height": 217
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-play_btn_group-mzzgrte0.png",
+					"frame": {
+						"x": 193,
+						"y": 347,
+						"width": 200,
+						"height": 217
+					}
+				},
+				"children": [
+					{
+						"objectId": "954F082D-9E17-4ADE-9737-96AFCBA25012",
+						"kind": "group",
+						"name": "btn_play_pause",
+						"originalName": "btn_play_pause",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 221,
+							"y": 386,
+							"width": 142,
+							"height": 142
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-btn_play_pause-otu0rja4.png",
+							"frame": {
+								"x": 221,
+								"y": 386,
+								"width": 142,
+								"height": 142
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "6E26A8DD-8E92-418E-BB16-3BF081E34EC4",
+						"kind": "group",
+						"name": "track_group",
+						"originalName": "track_group",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 193,
+							"y": 350,
+							"width": 200,
+							"height": 211
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-track_group-nkuynke4.png",
+							"frame": {
+								"x": 193,
+								"y": 350,
+								"width": 200,
+								"height": 211
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "85A5A97F-BFEA-411A-9FE9-6DC6E55DD217",
+						"kind": "group",
+						"name": "track_gutter",
+						"originalName": "track_gutter",
+						"maskFrame": {
+							"x": 0,
+							"y": 0,
+							"width": 123,
+							"height": 256
+						},
+						"layerFrame": {
+							"x": 173,
+							"y": 320,
+							"width": 123,
+							"height": 256
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-track_gutter-odvbnue5.png",
+							"frame": {
+								"x": 173,
+								"y": 320,
+								"width": 123,
+								"height": 256
+							}
+						},
+						"children": []
+					}
+				]
+			},
+			{
+				"objectId": "56AB5085-C684-49C5-9965-758EC32EE0F0",
+				"kind": "group",
+				"name": "track_list",
+				"originalName": "track_list",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 18,
+					"y": 27,
+					"width": 327,
+					"height": 708
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-track_list-ntzbqjuw.png",
+					"frame": {
+						"x": 18,
+						"y": 27,
+						"width": 327,
+						"height": 708
+					}
+				},
+				"children": [
+					{
+						"objectId": "A7621A41-E4C6-413A-8CFD-2010055110D8",
+						"kind": "group",
+						"name": "current_track",
+						"originalName": "current_track",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 33,
+							"y": 151,
+							"width": 294,
+							"height": 150
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-current_track-qtc2mjfb.jpg",
+							"frame": {
+								"x": 33,
+								"y": 151,
+								"width": 294,
+								"height": 150
+							}
+						},
+						"children": [
+							{
+								"objectId": "5C8F0510-FF44-4932-8D57-5F1D159E4920",
+								"kind": "group",
+								"name": "Stacked_Group",
+								"originalName": "Stacked Group",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 191,
+									"y": 157,
+									"width": 136,
+									"height": 83
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-Stacked_Group-num4rja1.png",
+									"frame": {
+										"x": 191,
+										"y": 157,
+										"width": 136,
+										"height": 83
+									}
+								},
+								"children": [
+									{
+										"objectId": "0BDBD54C-9B30-44D0-9CFF-419921404825",
+										"kind": "group",
+										"name": "Stacked_Group1",
+										"originalName": "Stacked Group",
+										"maskFrame": null,
+										"layerFrame": {
+											"x": 191,
+											"y": 222,
+											"width": 136,
+											"height": 18
+										},
+										"visible": true,
+										"metadata": {
+											"opacity": 1
+										},
+										"image": {
+											"path": "images/Layer-Stacked_Group-mejeqkq1.png",
+											"frame": {
+												"x": 191,
+												"y": 222,
+												"width": 136,
+												"height": 18
+											}
+										},
+										"children": []
+									},
+									{
+										"objectId": "6D66F97C-BAD8-47CD-BEC1-75959C36C86A",
+										"kind": "group",
+										"name": "Group1",
+										"originalName": "Group",
+										"maskFrame": null,
+										"layerFrame": {
+											"x": 192,
+											"y": 192,
+											"width": 74,
+											"height": 16
+										},
+										"visible": true,
+										"metadata": {
+											"opacity": 1
+										},
+										"image": {
+											"path": "images/Layer-Group-nkq2nky5.png",
+											"frame": {
+												"x": 192,
+												"y": 192,
+												"width": 74,
+												"height": 16
+											}
+										},
+										"children": []
+									}
+								]
+							}
+						]
+					}
+				]
+			}
+		]
+	}
+]
 if (DeviceComponent) {DeviceComponent.Devices["iphone-6-silver"].deviceImageJP2 = false};
-if (window.Framer) {window.Framer.Defaults.DeviceView = {"deviceScale":"fit","selectedHand":"","deviceType":"apple-iphone-7-black","contentScale":1,"hideBezel":true,"orientation":0};
+if (window.Framer) {window.Framer.Defaults.DeviceView = {"deviceScale":"fit","selectedHand":"","deviceType":"apple-iphone-7-black","contentScale":1,"hideBezel":false,"orientation":0};
 }
-if (window.Framer) {window.Framer.Defaults.DeviceComponent = {"deviceScale":"fit","selectedHand":"","deviceType":"apple-iphone-7-black","contentScale":1,"hideBezel":true,"orientation":0};
+if (window.Framer) {window.Framer.Defaults.DeviceComponent = {"deviceScale":"fit","selectedHand":"","deviceType":"apple-iphone-7-black","contentScale":1,"hideBezel":false,"orientation":0};
 }
 window.FramerStudioInfo = {"deviceImagesUrl":"\/_server\/resources\/DeviceImages","documentTitle":"009-music-player.framer"};
 
